1018c28084df330a1e4aefce2e02b962ba6ffe7f
Guo_X <guoxiang191@outlook.com>
2015-04-23 12:12:33 +0800

    init

---
+++ application/views/visit.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<title>天外天微投票</title>
+</head>
+<body>
+
+<div id="container">
+</div>
+
+</body>
+</html>
