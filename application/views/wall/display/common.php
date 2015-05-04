192c84dca23e45eb6f70ab57d5c0255b0ff4f43c
Guo_X <guoxiang191@outlook.com>
2015-05-04 17:23:51 +0800

    lottery updated

--- application/views/wall/display/common.php
+++ application/views/wall/display/common.php
@@ -4,6 +4,5 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=8" />
 	<meta charset="utf-8" />
 	<link rel="stylesheet" href="<?=base_url().'static/wall/display/css/common.css';?>" />
-	<link rel="stylesheet" href="<?=base_url().'static/wall/display/css/sprite-action-white.css';?>" />
 </head>
 <body>
